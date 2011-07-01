b97e2c40765395e7f721d1442c0dafd94d6c1b0e
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2011-07-01 18:48:33 +0800

    added simple demo demo.zul

--- src/org/zkoss/addon/absolutelayout/Absolutechildren.java
+++ src/org/zkoss/addon/absolutelayout/Absolutechildren.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.zkoss.addon.absolutelayout;
 
 import org.zkoss.zk.ui.Component;
@@ -8,22 +5,41 @@ import org.zkoss.zk.ui.UiException;
 import org.zkoss.zul.impl.XulElement;
 
 /**
- * @author Ashish
+ * <p>A container component that can contain any other ZK component and can only 
+ * be contained as direct child of Absolutelayout component. It can be absolutely positioned within 
+ * Absolutelayout component by either setting "x" and "y" attribute or calling {@link #setX(int)} and {@link #setY(int)} 
+ * methods.
  * 
+ * <p>Default {@link #getZclass}: z-absolutechildren.
+ * 
+ * @author ashish
+ * @since 0.8.0
  */
 public class Absolutechildren extends XulElement {
 
 	private int _X;
 	private int _Y;
 
+	/**
+	 * <p>sets current "x" position within parent container component.If negative 0 is assumed.
+	 * <p>Default: 0
+	 * @param x int
+	 */
 	public void setX(int x) {
+		if (x < 0) x = 0;
 		if (this._X != x) {
 			this._X = x;
 			smartUpdate("x", this._X);
 		}
 	}
 
+	/**
+	 * sets current "y" position within parent container component. If negative 0 is assumed.
+	 * <p>Default: 0
+	 * @param y int
+	 */
 	public void setY(int y) {
+		if (y < 0) y = 0;
 		if (this._Y != y) {
 			this._Y = y;
 			smartUpdate("y", this._Y);
@@ -38,10 +54,18 @@ public class Absolutechildren extends XulElement {
 		render(renderer, "y", this._Y);
 	}
 
+	/**
+	 * return current "x" position within parent container component
+	 * @return int
+	 */
 	public int getX() {
 		return this._X;
 	}
 
+	/**
+	 * returns current "y" position within parent container component
+	 * @return
+	 */
 	public int getY() {
 		return this._Y;
 	}
@@ -51,7 +75,7 @@ public class Absolutechildren extends XulElement {
 			throw new UiException("Wrong parent: " + parent);
 		super.beforeParentChanged(parent);
 	}
-	
+
 	public String getZclass() {
 		return (this._zclass != null ? this._zclass : "z-absolutechildren");
 	}
--- src/org/zkoss/addon/absolutelayout/Absolutelayout.java
+++ src/org/zkoss/addon/absolutelayout/Absolutelayout.java
@@ -4,8 +4,16 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.UiException;
 import org.zkoss.zul.impl.XulElement;
 
-public class Absolutelayout extends XulElement  {
-
+/**
+ * <p>An Absolutelayout component can contain absolute positioned multiple
+ * absolutechildren components.
+ * 
+ * <p>Default {@link #getZclass}: z-absolutelayout.
+ * 
+ * @author ashish
+ * @since 0.8.0
+ */
+public class Absolutelayout extends XulElement {
 
 	public void beforeChildAdded(Component child, Component refChild) {
 		if (!(child instanceof Absolutechildren))
